1f25b5b04854f43caf3e4c2a8d351ca43f8ba3d8
Naresh Gupta <nareshgupta0899@gmail.com>
2024-03-26 23:37:13 +0530

    signup and login page added

--- jobsol-frontend/src/App.js
+++ jobsol-frontend/src/App.js
@@ -1,25 +1,19 @@
-import logo from './logo.svg';
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import SignupLogin from "../src/component/SignupLogin";
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    
+
+    <BrowserRouter>
+      <Routes>
+        <Route path="/signup" element={<SignupLogin />} />
+      </Routes>
+    
+    </BrowserRouter>
+
+    );
 }
 
 export default App;
