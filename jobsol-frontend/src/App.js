7e447a5408faeb4a9d18a6975926532645b11b16
princymittal <princeemittal1507@gmail.com>
2024-04-01 15:24:17 +0530

    forgot and reset password design

--- jobsol-frontend/src/App.js
+++ jobsol-frontend/src/App.js
@@ -1,19 +1,20 @@
-import {BrowserRouter, Route, Routes} from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import SignupLogin from "../src/component/SignupLogin";
-import './App.css';
+import ForgotPassword from "../src//component/ForgotPassword";
+
+import "./App.css";
+import ResetPage from "./component/ResetPage";
 
 function App() {
   return (
-    
-
     <BrowserRouter>
       <Routes>
         <Route path="/signup" element={<SignupLogin />} />
+        <Route path="/forgot-password" element={<ForgotPassword />} />
+        <Route path="/reset-password" element={<ResetPage />} />
       </Routes>
-    
     </BrowserRouter>
-
-    );
+  );
 }
 
 export default App;
